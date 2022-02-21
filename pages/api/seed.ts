797e9aad73cb2faa5d566003a5785f7c0986333a
Thibault Dilman <thibault.dilman@protonmail.com>
2022-02-21 16:30:38 +0400

    users added to the data base

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -4,6 +4,7 @@ import nc from 'next-connect';
 import dbConnect from '../../lib/dbConnect';
 import Product from '../../models/Product';
 import data from '../../app/utils/data';
+import User from '../../models/User';
 
 const handler = nc();
 
@@ -12,6 +13,8 @@ handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         await Product.deleteMany();
         await Product.insertMany(data.products);
+        await User.deleteMany();
+        await User.insertMany(data.users);
         res.status(200).json({ message: 'seeded successfully' });
     } catch (err) {
         console.log(err);
--- typings.d.ts
+++ typings.d.ts
@@ -20,20 +20,17 @@ export interface ProductArray {
     products: ProductType[];
 }
 
-export interface PayloadType {
+export interface UserType {
     _id: string;
     name: string;
-    slug: string;
-    category: string;
-    image: string;
-    price: number;
-    brand: string;
-    rating: number;
-    numReviews: number;
-    countInStock: number;
-    description: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
     __v: number;
     createdAt: string;
     updatedAt: string;
-    quantity: number;
+}
+
+export interface UserArray {
+    users: UserType[];
 }
