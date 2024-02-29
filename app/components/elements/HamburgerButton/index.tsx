3fc0a5dda72f97c70510442d55fee3e41ee4504b
Thibault D <thibault.dilman@tuta.io>
2024-02-29 10:10:09 +0100

    made hamburger menu disapear on Open on homepage

--- app/components/elements/HamburgerButton/index.tsx
+++ app/components/elements/HamburgerButton/index.tsx
@@ -1,54 +1,51 @@
-import { ReactChild, ReactChildren, useState } from 'react';
-import styles from './HamburgerButton.module.scss';
+import { useState } from "react";
+import styles from "./HamburgerButton.module.scss";
 
 const HamburgerButton = (props: any) => {
-    const [active, setActive] = useState(false);
-    const isActive = active ? styles.active : styles.notActive;
+  const [active, setActive] = useState(false);
+  const isActive = active ? styles.active : styles.notActive;
 
-    // function to close the side panel while user click outside of it //
-    const onClickOutside = (e: any) => {
-        e.preventDefault();
-        if (e.target === e.currentTarget) {
-            setActive(false);
-            return;
-        }
-        return;
-    };
+  // function to close the side panel while user click outside of it //
+  const onClickOutside = (e: any) => {
+    e.preventDefault();
+    if (e.target === e.currentTarget) {
+      setActive(false);
+      return;
+    }
+    return;
+  };
 
-    return (
-        <>
-            <div className={`${styles.hamburger} ${active && styles.active}`}>
-                <div
-                    className={`${styles.btn} ${isActive}`}
-                    onClick={() => setActive((prev) => !prev)}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </div>
+  return (
+    <>
+      <div className={`${styles.hamburger} ${active && styles.active}`}>
+        <div
+          className={`${styles.btn} ${isActive} ${styles.btnOut}`}
+          onClick={() => setActive((prev) => !prev)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={`${styles.main} ${isActive}`} onClick={onClickOutside}>
+        <div className={`${styles.sideBar} ${isActive}`}>
+          <header className={styles.title}>
+            <h3>Shopping by category</h3>
             <div
-                className={`${styles.main} ${isActive}`}
-                onClick={onClickOutside}
+              className={`${styles.btn} ${isActive}`}
+              onClick={() => setActive((prev) => !prev)}
             >
-                <div className={`${styles.sideBar} ${isActive}`}>
-                    <header className={styles.title}>
-                        <h3>Shopping by category</h3>
-                        <div
-                            className={`${styles.btn} ${isActive}`}
-                            onClick={() => setActive((prev) => !prev)}
-                        >
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </header>
-                    <hr className={styles.separator} />
-                    {props.children}
-                </div>
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
-        </>
-    );
+          </header>
+          <hr className={styles.separator} />
+          {props.children}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HamburgerButton;
